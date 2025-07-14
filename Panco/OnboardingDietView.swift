4e0e1c12dfa2b95d784aea9f38597c06dc6c5e01
FrederickHadi <s3953344@student.rmit.edu.au>
2025-07-15 06:57:39 +1000

    Added navigation from NewPlanView to PlanningRecipeView

---
+++ Panco/OnboardingDietView.swift
@@ -0,0 +1,74 @@
+//OnboardingDietView - 1st page the user will see when they first open the app.
+// created by Avia
+// last edited Monday 14th July 9:39pm
+
+
+import SwiftUI
+
+struct OnboardingDietView: View {
+    @State private var selectedTags: Set<String> = []
+    
+    let tags = ["Vegan", "Vegetarian", "Pescetarian", "Keto", "Halal", "Kosher"]
+    
+    var body: some View {
+        ZStack {
+            Color(.pancoNeutral)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Diet")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.horizontal, 40)
+                    .padding(.top, 50)
+                
+                ScrollView {
+                    Spacer()
+                    
+                    //‼️ need to remember tags to put into preferences so that the user can change it as well as to recommend recipes that fit their dietary requirements
+                    
+                    ForEach(tags, id: \.self) { tag in
+                        Button(action: {
+                            if selectedTags.contains(tag) {
+                                selectedTags.remove(tag)
+                            } else {
+                                selectedTags.insert(tag)
+                            }
+                        }) {
+                            Text(tag)
+                                .foregroundColor(selectedTags.contains(tag) ? .white : Color(red: 0.21, green: 0.55, blue: 0.39))
+                                .font(.title3)
+                                .padding(.vertical, 15)
+                                .padding(.horizontal, 12)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .background(selectedTags.contains(tag) ? Color.pancoGreen : Color.clear)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 24)
+                                        .stroke(.pancoGreen, lineWidth: 2)
+                                )
+                                .cornerRadius(24)
+                        }
+                        .padding(.horizontal, 30)
+                    }
+                }
+                
+                Spacer()
+                
+                Button("Continue") {}
+                    .foregroundColor(.white)
+                    .font(.headline)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color(red: 0.52, green: 0.62, blue: 0.56))
+                    .cornerRadius(20)
+                    .shadow(radius: 5)
+                    .padding(.horizontal, 100)
+                    .padding(.bottom, 30)
+            }
+        }
+    }
+}
+#Preview {
+    OnboardingDietView()
+}
+
--- Panco/PlanningConstraintsView.swift
+++ Panco/PlanningConstraintsView.swift
@@ -1,6 +1,6 @@
-
-// Version: 14 July 11.30AM
-// By Nikki
+// PlanningConstraintsView: User inputs their availability and requirements
+// Created by: Nikki on 14 July
+// Last edited by: Nikki on 14 July 9.30PM (Sent)
 
 import SwiftUI
 
@@ -13,134 +13,161 @@ struct PlanningConstraintsView: View {
     
     
     var body: some View {
-        
-        ZStack {
-            Color(.pancoNeutral)
-                .ignoresSafeArea()
+        NavigationStack {
             
-            VStack{
+            
+            ZStack {
+                Color(.pancoNeutral)
+                    .ignoresSafeArea()
                 
-                ProgressView(value: 0.9)
-                    .frame(width: 160)
-                    .accentColor(Color.pancoGreen)
-                
-                // ⭐️ Page title
-                Text("Planning")
-                    .fontWeight(.bold)
-                    .font(.largeTitle)
-                    .padding(.trailing, 180)
-                    .padding(.top, 10)
-                
-                
-                // ⭐️ Number of recipe
                 VStack{
-                    Text("No. of recipes this week")
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                        .padding(.top, 10)
                     
+                    // Progress bar
                     HStack{
-                        // - button
-                        Button {
-                            if noRecipes > 0 {
-                                noRecipes -= 1
-                            }
-                        } label: {
-                            Image(systemName: "minus.circle.fill")
-                                .resizable()
-                                .scaledToFit()
-                            
-                                .frame(width: 40)
-                                .foregroundStyle(Color.pancoGreen)
-                            
-                            
-                            
-                            // recipe count
-                            Text("\(Int(noRecipes))")
-                                .fontWeight(.bold)
-                                .font(.largeTitle)
-                                .padding(.leading, 10)
-                                .padding(.trailing, 10)
-                                .foregroundStyle(.white)
-                                .frame(maxWidth:80, maxHeight: 80)
-                                .background(.pancoGreen)
-                                .cornerRadius(15)
-                                .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
-                                .padding()
-                            
-                            
-                            // + button
-                            Button{
-                                if noRecipes < 10 {
-                                    noRecipes += 1
-                                }
-                            } label: {
-                                Image(systemName: "plus.circle.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .foregroundStyle(Color.pancoGreen)
-                                    .frame(width: 40)
-                            }
-                        }//HStack end
-                        
-                    }//VStack end
-                    
-                }// VStack no. recipe section end
-                .frame(maxWidth: .infinity, maxHeight: 180)
-                .background(.pancoLightRed.opacity(0.15))
-                .cornerRadius(15)
-                .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
-                .padding()
-                
-                
-                // ⭐️ Time scroll
-                VStack{
-                    
-                    Text("Max. time per cooking session (minutes)")
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                        .frame(width: 300, height: 50)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                    
-                    
-                    
-                    Picker("Max Cooking Time", selection: $maxCookingTime)
-                    {
-                        // Generate any range you like—here, 1 min up to 120 min.
-                        ForEach(1...120, id: \.self)
-                        {
-                            minute in
-                            Text("\(minute)").tag(minute)
-                        }
-                        
+                        Spacer()
+                        ProgressView(value: 0.33)
+                            .frame(width: 160)
+                            .accentColor(Color.pancoGreen)
+                        Spacer()
                     }
                     
-                    .pickerStyle(.wheel)// gives you the spinning “slot‑machine” look
-                    .background(Color.pancoNeutral)
-                    .frame(width:350, height: 100)// optional: keeps the wheel a nice compact height
-                    .cornerRadius(20)
-                }
-                .frame(maxWidth: .infinity, maxHeight: 250)
-                .background(.pancoLightRed.opacity(0.15))
-                .cornerRadius(15)
-                .padding()
-                
-                // ⭐️ Continue button
-                Button("Continue") {
-                    //‼️ Add navigation to PlanningRecipeView
-                }
-                .foregroundColor(.pancoNeutral)
-                .font(.headline)
-                .padding()
-                .frame(width: 180, height: 60)
-                .background(.pancoLightGreen)
-                .cornerRadius(20)
-                .padding(.top,20)
-                .padding(.bottom, 50)
-                .shadow(radius: 5)
-            }//VStack end
-        }//ZStack end
+                    // ⭐️ Page title
+                    Text("Planning")
+                        .fontWeight(.bold)
+                        .font(.largeTitle)
+                        .padding(.trailing, 180)
+                        .padding(.top, 20)
+                    
+                    
+                    // ⭐️ Number of recipe
+                    VStack{
+                        Text("No. of recipes this week")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .padding(.top, 10)
+                        
+                        HStack{
+                            // - button
+                            Button {
+                                if noRecipes > 0 {
+                                    noRecipes -= 1
+                                }
+                            } label: {
+                                Image(systemName: "minus.circle.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                
+                                    .frame(width: 40)
+                                    .foregroundStyle(Color.pancoGreen)
+                                
+                                
+                                
+                                // recipe count
+                                Text("\(Int(noRecipes))")
+                                    .fontWeight(.bold)
+                                    .font(.largeTitle)
+                                    .padding(.leading, 10)
+                                    .padding(.trailing, 10)
+                                    .foregroundStyle(.white)
+                                    .frame(maxWidth:80, maxHeight: 80)
+                                    .background(.pancoGreen)
+                                    .cornerRadius(15)
+                                    .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
+                                    .padding()
+                                
+                                
+                                // + button
+                                Button{
+                                    if noRecipes < 10 {
+                                        noRecipes += 1
+                                    }
+                                } label: {
+                                    Image(systemName: "plus.circle.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .foregroundStyle(Color.pancoGreen)
+                                        .frame(width: 40)
+                                }
+                            }//HStack end
+                            
+                        }//VStack end
+                        
+                    }// VStack no. recipe section end
+                    .frame(maxWidth: .infinity, maxHeight: 180)
+                    .background(.pancoLightRed.opacity(0.15))
+                    .cornerRadius(15)
+                    .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
+                    .padding()
+                    
+                    
+                    // ⭐️ Time scroll
+                    VStack{
+                        
+                        Text("Max. time per cooking session (minutes)")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .frame(width: 300, height: 50)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                        
+                        
+                        
+                        Picker("Max Cooking Time", selection: $maxCookingTime)
+                        {
+                            // Generate any range you like—here, 1 min up to 120 min.
+                            ForEach(1...120, id: \.self)
+                            {
+                                minute in
+                                Text("\(minute)").tag(minute)
+                            }
+                            
+                        }
+                        
+                        .pickerStyle(.wheel)// gives you the spinning “slot‑machine” look
+                        .background(Color.pancoNeutral)
+                        .frame(width:350, height: 100)// optional: keeps the wheel a nice compact height
+                        .cornerRadius(20)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 250)
+                    .background(.pancoLightRed.opacity(0.15))
+                    .cornerRadius(15)
+                    .padding()
+                    
+                    //                // ⭐️ Continue button
+                    //                Button("Continue") {
+                    //                    //‼️ Add navigation to PlanningRecipeView
+                    //                }
+                    //                .foregroundColor(.pancoNeutral)
+                    //                .font(.headline)
+                    //                .padding()
+                    //                .frame(width: 180, height: 60)
+                    //                .background(.pancoLightGreen)
+                    //                .cornerRadius(20)
+                    //                .padding(.top,20)
+                    //                .padding(.bottom, 50)
+                    //                .shadow(radius: 5)
+                    
+                    // ⭐️ Continue button
+                    NavigationLink {
+                        PlanningRecipeView()  // ← Destination
+                    } label: {
+                        Text("Continue")      // ← Label with button styling
+                            .foregroundColor(.pancoNeutral)
+                            .font(.headline)
+                            .padding()
+                            .frame(width: 180, height: 60)
+                            .background(.pancoLightGreen)
+                            .cornerRadius(20)
+                            .padding(.top, 20)
+                            .padding(.bottom, 50)
+                            .shadow(radius: 5)
+                    }
+                    
+                    
+                    
+                }//VStack end
+            }//ZStack end
+        }//NavigationStack end
     } //body end
 }//struct end
 
