7d2ba384c6dc9256cea6766b4800f3bd65f0b855
FrederickHadi <s3953344@student.rmit.edu.au>
2025-07-17 11:32:58 +1000

    added under construction preferences view

---
+++ Panco/PreferencesView.swift
@@ -0,0 +1,27 @@
+//
+//  PreferencesView.swift
+//  Panco
+//
+//  Created by Erick Hadi on 17/7/2025.
+//
+
+import SwiftUI
+
+struct PreferencesView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Under construction!")
+                .font(.largeTitle)
+            
+            Image("PancoConstruction")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 200)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    PreferencesView()
+}
